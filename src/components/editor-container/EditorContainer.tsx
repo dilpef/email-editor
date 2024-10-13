a34db783aa56bcddaed1c8c0077f5911fbb8fb2f
Ilya Petrenko <ilya.petrenko.96@mail.ru>
2024-10-13 17:52:47 +0300

    Начальная структура проекта

---
+++ src/components/editor-container/EditorContainer.tsx
@@ -0,0 +1,9 @@
+import styles from "./EditorContainer.module.css";
+
+export function EditorContainer() {
+  return (
+    <>
+      <div className={styles.editorContainerBody}>Редактор письма</div>
+    </>
+  );
+}
